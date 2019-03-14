34ce4796bc68fa0d30d6af88ba885dafa0cd7e80
kristina.argirova <kristina.argirova@betlab.com>
2019-03-15 01:40:25 +0200

    add draft to think over

--- task1/task1.py
+++ task1/task1.py
@@ -1,13 +1,28 @@
+class GeneralCounter:
+
+    def __init__(self, id=0):
+        self.id = id
+ 
+class Id:
+    id = GeneralCounter()
+ 
+class Account(Id):
+    pass
+ 
+class Vehicle(Id):
+    pass
+ 
 class Player(Account):
     def __init__(self):
         object_id_collector = self.id
-
+ 
 class Bot(Account):
     def __init__(self):
         object_id_collector = self.id
-
+ 
 class Tank(Vehicle):
     def __init__(self):
         object_id_collector = self.id
 
+ 
 print Player().id, Player().id, Tank().id, Bot().id, Player().id
